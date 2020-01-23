d9fe600c4dd16dcc1dd1947a0c38dda4a7894c18
Eize Hofstra <Jelle.Hofstra-CIC.Netherlands@ibm.com@eizes-air.groningen.nl.ibm.com>
2020-01-23 13:51:42 +0100

    added server.js and database info

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const morgan = require('morgan');
+app.use(morgan.dev());
+
+const cors = require('cors');
+app.use(cors());
+
+const errorhandler = require('errorhandler');
+app.use(errorhandler());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at port: ${PORT}`);
+})
+
+module.exports = app;
